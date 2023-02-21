6eb2b65b5997874de9f384c56a527d4fee8bca07
Krystina Witt <krystinawitt@gmail.com>
2023-02-21 15:31:43 +0100

    iteration 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// src/App.js
 import './App.css'
+import contacts from "./contacts.json"
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const [contactList, setcontactList] = useState(contacts.slice(0,5))
+const [remainingContacts, setRemainingContacts] = useState(contacts.slice(6))
+
+
+const addRandom = () =>{
+
+  const randomIndex = Math.floor(Math.random() * (remainingContacts.length))
+  const randomContact = remainingContacts[randomIndex]
+  setcontactList([...contactList, randomContact])
+  let filteredArr = remainingContacts.filter((el, index) =>{
+    return el.name !== randomContact.name
+  })
+  setRemainingContacts(filteredArr)
+  console.log("contactList:", contactList)
+  console.log("remainingContacts:", remainingContacts)
 }
 
+  return <div className='App'>
+
+<table>
+  <thead>
+   <tr>
+   <th>Picture</th>
+    <th>Name</th>
+    <th>Popularity</th>
+    <th>Won an Oscar</th>
+    <th>Won an Emmy</th>
+   </tr>
+  </thead>
+  <tbody>
+{contactList.map(celebrity => (
+  <tr key={celebrity.name}>
+    <td><img width="50" alt={celebrity.name} src={celebrity.pictureUrl}></img></td>
+    <td>{celebrity.name}</td>
+    <td>{celebrity.popularity.toFixed(2)}</td>
+    <td>{celebrity.wonOscar ? "🏆" : ""}</td>
+    <td>{celebrity.wonEmmy ? "🏆" : ""}</td>
+  </tr>
+))}
+
+  </tbody>
+  </table>
+
+  <button onClick={addRandom}>Add Random Contact</button>
+
+  </div>
+}
 export default App
